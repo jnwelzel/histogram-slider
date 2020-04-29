e926e508573ed70e372439bd1bef383737337215
Jonathan Welzel <jnwelzel@gmail.com>
2020-04-29 10:36:11 +0200

    rheostat ok

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Rheostat from "rheostat";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Rheostat</h1>
+      <Rheostat min={1} max={100} values={[1, 100]} />
     </div>
   );
 }
