f05e2de047b7d3d69811da3c7161d39c90da99e8
Jonathan Welzel <jnwelzel@gmail.com>
2020-05-14 13:56:11 +0200

    Updates

---
+++ precache-manifest.eb6284570e0c54d2170eb297bf4b8927.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56db825d981be236598f65173861ddcd",
+    "url": "/histogram-slider/index.html/index.html"
+  },
+  {
+    "revision": "b100be9bd2cf23c85ce7",
+    "url": "/histogram-slider/index.html/static/css/main.3aa550a4.chunk.css"
+  },
+  {
+    "revision": "e2deab29edc06e1d3810",
+    "url": "/histogram-slider/index.html/static/js/2.fd46f48f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/histogram-slider/index.html/static/js/2.fd46f48f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b100be9bd2cf23c85ce7",
+    "url": "/histogram-slider/index.html/static/js/main.69378a67.chunk.js"
+  },
+  {
+    "revision": "4e821f90114907da1fc0",
+    "url": "/histogram-slider/index.html/static/js/runtime-main.7d372701.js"
+  }
+]);
