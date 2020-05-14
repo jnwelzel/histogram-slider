cd8f742382fcd44464c5cfc65db6ca049211a68c
Jonathan Welzel <jnwelzel@gmail.com>
2020-05-14 13:47:55 +0200

    Updates

--- README.md
+++
@@ -1,4 +0,0 @@
-## Check it out
-1. Git clone
-2. `yarn`
-3. `yarn start`
---
+++ precache-manifest.a091f06a01b58e0b2babca28c0ea2140.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d3da54404e8937b49867c9a6352ca52",
+    "url": "/histogram-slider/index.html"
+  },
+  {
+    "revision": "a06921e22e07635f2a6b",
+    "url": "/histogram-slider/static/css/main.3aa550a4.chunk.css"
+  },
+  {
+    "revision": "e2deab29edc06e1d3810",
+    "url": "/histogram-slider/static/js/2.fd46f48f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/histogram-slider/static/js/2.fd46f48f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a06921e22e07635f2a6b",
+    "url": "/histogram-slider/static/js/main.c8f07051.chunk.js"
+  },
+  {
+    "revision": "46b24be2d77f4218ff0d",
+    "url": "/histogram-slider/static/js/runtime-main.e73aa591.js"
+  }
+]);
